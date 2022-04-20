464976b4a1941d6c84bea41ebfb5bb4bd1e9d9d4
Fizzify <fizzyplayzoriginal5@gmail.com>
2022-04-21 07:14:31 +0800

    Added source.

---
+++ game.js
@@ -0,0 +1,74 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#app-level").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function playSound(name) {
+  var audio = new Audio("assets/sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#app-level").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeIn(200)
+    .fadeOut(200)
+    .fadeIn(200);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setInterval(() => {
+    $("#" + currentColour).removeClass("pressed");
+  }, 200);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("correct");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    startOver();
+    $("#app-level").text("Game Over, Press Any Key to Restart");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    var wrong = new Audio("assets/sounds/wrong.mp3");
+    wrong.play();
+  }
+}
+
+function startOver() {
+  started = false;
+  level = 0;
+  gamePattern = [];
+}
